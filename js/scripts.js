fcef6f9d7ba796dd0f466d200410417dbb4d1c7d
Lucas Pereira Santiago <lucaspereirasantiago@hotmail.com>
2022-04-09 16:25:50 -0300

    inicio do script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log('teste');
+const inputNome = document.querySelector('#nome');
+const inputLink = document.querySelector('#link');
+const btn = document.querySelector('#organizar');
+const errorMsg = document.querySelector('#errorMsg');
+
+btn.addEventListener("click", (event) => {
+    
+});
