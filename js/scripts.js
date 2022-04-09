d16ddd3b90ea1cbc551a684635be1de7dc2d68ee
Lucas Pereira Santiago <lucaspereirasantiago@hotmail.com>
2022-04-09 16:29:45 -0300

    errorMsg inicio

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,11 @@ const btn = document.querySelector('#organizar');
 const errorMsg = document.querySelector('#errorMsg');
 
 btn.addEventListener("click", (event) => {
-    
+    event.preventDefault();
+    nomeValue = inputNome.value;
+    linkValue = inputLink.value;
+
+    if(nomeValue === "" || linkValue === ""){
+        errorMsg.textContent = "Preencha os campos!";
+    }
 });
