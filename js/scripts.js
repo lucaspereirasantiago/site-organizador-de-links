b0a25b60653e6dc4b80e4e56cda98f5c5eb1ffa5
Lucas Pereira Santiago <lucaspereirasantiago@hotmail.com>
2022-04-11 23:01:47 -0300

    adicionando else

--- js/scripts.js
+++ js/scripts.js
@@ -11,26 +11,25 @@ btn.addEventListener("click", (event) => {
 
     if(nomeValue === "" || linkValue === ""){
         errorAlert.textContent = "Preencha os campos!";
-    }
+    } else {
+        const divSections = document.createElement("ul");
+        divSections.classList = "blocks";
+
+        divSections.innerHTML =
+        `<li class="sections">
+            <div class="block-conteudo">
+                <h3 class="titulo-conteudo">${nomeValue}</h3>
+                <button class="link-conteudo"><a href="${linkValue}"
+                target="_blank">Ir para o link</a></button>
+            </div>
+        </li>`
+    
+        sections.appendChild(divSections);
+    };
     
     setTimeout(() => {
         errorAlert.textContent = "";    
     }, 3000);
-
-
-    const divSections = document.createElement("ul");
-    divSections.classList = "blocks";
-
-    divSections.innerHTML =
-    `<li class="sections">
-        <div class="block-conteudo">
-            <h3 class="titulo-conteudo">${nomeValue}</h3>
-            <button class="link-conteudo"><a href="${linkValue}"
-            target="_blank">Ir para o link</a></button>
-        </div>
-    </li>`
-    
-    sections.appendChild(divSections);
 
 
     inputNome.value = '';
